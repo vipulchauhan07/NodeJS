6a3a0c502ade20e9e4e8005abb0324e43358e2cb
Vipul Chauhan <viratdev07@gmail.com>
2020-05-25 23:38:03 +0530

    Create Socket.io event

--- 05_01/server.js
+++ 05_01/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -23,6 +25,11 @@ app.post('/messages', (req, res) => {
   res.sendStatus(200);
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('A user connected');
+})
+
+
+var server = http.listen(3000, () => {
   console.log('Server is listening on port ' + server.address().port);
 });
