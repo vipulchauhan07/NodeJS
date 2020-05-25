cd0430dd81b67519d7c95275577a5b6dfc4c754f
Vipul Chauhan <viratdev07@gmail.com>
2020-05-25 23:21:17 +0530

    Creating a post service

--- 05_01/server.js
+++ 05_01/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
 
 var messages = [
   { name: 'Bob', message: 'Hi' },
@@ -13,7 +17,11 @@ app.get('/messages', (req, res) => {
   res.send(messages);
 })
 
-
+app.post('/messages', (req, res) => {
+  //  console.log(req.body);
+  messages.push(req.body);
+  res.sendStatus(200);
+})
 
 var server = app.listen(3000, () => {
   console.log('Server is listening on port ' + server.address().port);
