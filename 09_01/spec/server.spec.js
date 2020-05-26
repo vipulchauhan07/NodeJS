eb78dc0bf5a128e8bf107d8f213291fca3a4d754
Vipul Chauhan <viratdev07@gmail.com>
2020-05-26 23:55:02 +0530

    Jasmine testing

---
+++ 09_01/spec/server.spec.js
@@ -0,0 +1,21 @@
+var request = require('request')
+describe('calc', () => {
+  it('should multilpy 2 and 2', () => {
+    expect(2*2).toBe(4);
+  })
+})
+
+describe('get messages', () => {
+  it('should return 200 Ok', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) => {
+      expect(res.statusCode).toEqual(200)
+      done();
+    })
+  })
+  it('should return a list , that is not empty', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) => {
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+      done();
+    })
+  })
+})
